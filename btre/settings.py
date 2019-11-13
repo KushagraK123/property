e6c50b62c9350a960feb9a11cca30cd3bedf15d7
_kushagra_ <kushagrakasliwal47@gmail.com>
2019-11-13 19:04:39 +0530

    fixed errors

--- btre/settings.py
+++ btre/settings.py
@@ -144,12 +144,7 @@ MESSAGE_TAGS = {
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'empyrealgamesindia@gmail.com'
-EMAIL_HOST_PASSWORD = 'jtaahsmppyqafxnq'
+
 
 
 
